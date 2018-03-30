209c3807c464fdee747bc396a1bc8518a194d353
alu0100813272 <alu0100813272@ull.edu.es>
2018-03-30 00:27:17 +0000

    otroa

--- src/fraccion/Fraccion.java
+++ src/fraccion/Fraccion.java
@@ -1,4 +1,8 @@
 package fraccion;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Clase que almacenas el numerado y denominador. Tambien se hara operaciones
  * como si de una fraccion se tratase.
@@ -9,30 +13,122 @@ package fraccion;
  * @version 1.0
  * @since 2018-03-14
  */
-public class Fraccion {
+public class Fraccion implements Cloneable{
 	private long numerador;
 	private long denominador;
-	
+	public Object clone(){
+		   Fraccion obj = null;
+	        try{
+	            obj=(Fraccion)super.clone();
+	        }catch(CloneNotSupportedException ex){
+	            System.out.println(" no se puede duplicar");
+	        }
+	        
+	        return obj;
+	 } 
+	/**
+	 * Constructor por defecot crea la fraccíon 0/1;
+	 */
 	Fraccion(){
 		numerador = 0L;
 		denominador = 1L;
 	}
-	
+	/**
+	 * Constructor que se le pasa solo el numerador de forma entera, 
+	 * el denominado va ser siempre 1
+	 * @param numerador
+	 */
+	Fraccion(long numerador){
+		this();
+		this.numerador = numerador;
+	}
 	/**
 	 * Contructor que le pasaremos dos valores el numerador y denominador.
 	 * @param numerador
 	 * @param denominador
 	 */
 	Fraccion(long numerador, long denominador) throws IllegalArgumentException{
+		this(numerador);
 		if(denominador == 0){
 			throw new IllegalArgumentException( "Denominador igual 0 FracciÃ³n no se construlle" );
 		}
-		
-		this.numerador = numerador;
-		this.denominador = denominador;
-		normalizarSigno();
-		simplificar();
+		// Si numerado es 0 siempre es 0/1 que es la fracción por defecto ya se ha creado antes
+		if(numerador != 0) {
+			this.denominador = denominador;
+			simplificar();
+			normalizarSigno();
+		}
 	}
+	/**
+	 * Método que crea la fracciòn decimal irreducible equvalente al número decimal pasado 
+	 * como argumetno.
+	 * @param num
+	 */
+    Fraccion(double num){
+    	this( (long)(num*(long)Math.pow(10, contDecimal( "" + num))), (long)Math.pow(10, contDecimal( "" + num))); 
+    }
+    /**
+     * Devulve la cantidad de decimales que hay en el estrin que expresa un decimal
+     * ejemplo le pasamo "2.33"  devolvera dos.
+     * @param dato
+     * @return
+     */
+   private static long contDecimal(String dato) {
+    	Pattern pat = Pattern.compile("[.]\\d*$");
+        Matcher mat = pat.matcher(dato);
+        return mat.find() ? (mat.group().length()-1) : 0;
+    }
+    /**
+	 * Método que crea la fracción decimal irreducible equvalente al número decimal pasado 
+	 * como argumetno y es de tipo string se puede pasar "#" o  "#/#".
+	 * @param num
+	 */
+    Fraccion(String num){
+    	setFraction(parseFraction(num)); //hacer un clonado.??? nira si devuelve
+    }
+    
+    /**
+     * Método que analiza el string que se la pasa y devuelve una fraccion.
+     * @param s
+     * @return
+     */
+    public static Fraccion parseFraction(String num) {
+    		Fraccion result = null;
+    	    if(isDecimal(num)) {
+    	    	result = new Fraccion(Double.parseDouble(num));
+    		}
+    	    if(isEntero(num)) {
+    	    	result = new Fraccion(Long.parseLong(num));
+    	    }
+    	    if(isFraccion(num)) {
+    	    	String[] aux = num.split("/");
+    	    	result = new Fraccion( Long.parseLong(aux[0]), Long.parseLong(aux[1]));
+    	    }
+    	    
+			return result;
+    }
+    /**
+     * Método que detecta si es un decimal de tipo 7.8 o +7.8 o -7.9...
+     * tembien se puede separar con ,.
+     * */
+    public static Boolean isDecimal(String num)
+    {
+    	 return Pattern.matches( "^[-+]?\\d*(\\.|\\,)\\d*$", num.replaceAll("\\s*",""));
+    }
+    /**
+     * Método que detecta si es un valor entero positivo o negativo 3 o -3 o +4.
+     * */
+    public static Boolean isEntero(String num)
+    {
+    	 return Pattern.matches( "^[-+]?\\d*$", num.replaceAll("\\s*",""));
+    }
+    /**
+     * Método que detecta si es un valor en fracion como pro ejemplo 2/2 o +2/-1.
+     * */
+    public static Boolean isFraccion(String num)
+    {
+    	 return Pattern.matches( "^[-+]?\\d*[/][-+]?\\d*$", num.replaceAll("\\s*",""));
+    }
 	/**
 	 * MÃ©todo que comprueba que el valor del denominador 
 	 * sea negativo y normalizamos el signo.
@@ -40,13 +136,21 @@ public class Fraccion {
 	private void normalizarSigno(){
 		// si es negativo el denominador normalizamos signo.
 		if(denominador < 0 ){
-			this.numerador *= -1;
+			changeSing();
 			this.denominador *= -1;
 		}
 	}
-	
+	/**
+	 * Método para cambiar el signo de la fraccion.
+	 */
+	public void changeSing(){
+			this.numerador *= -1;
+	}
+	/**
+	 * Método que simplifica la fracción
+	 */
 	private void simplificar(){
-		long mcd = mcd(numerador,denominador);
+		long mcd = gcd(numerador,denominador);
 		numerador/=mcd;
 		denominador/=mcd;
 	}
@@ -57,19 +161,67 @@ public class Fraccion {
 	 * @param numB
 	 * @return el valor mcd calculado.
 	 */
-	public static long mcd(long valorA, long valorB){
-		//long valorA=numA, valorB=numB, aux=0;
-		long aux = 0;
-		while( valorA > 0){
-			aux = valorA;
-			valorA = valorB % valorA;
-			valorB = aux;
-	    }  
-		return valorB;
+	public static long gcd(long valorA, long valorB){
+		long a,b,resto = 1;
+		//Verifica que numerado es mayor.
+		a = numeroMayor(valorA,valorB);
+		b = numeroMenor(valorA,valorB);	
+		resto = a % b;
+	    while(resto != 0) { 
+            a = b;
+			b = resto;
+			resto = a % b;
+	    }
+			
+		return b;
 	}
-	
+	public static long lcm(long valorA, long valorB){
+        long mcm = 0;
+        // seleccionamos el mayor
+        long a = Math.max(valorA, valorB);
+        long b = Math.min(valorA, valorB);
+        mcm =  (a*b)/gcd(a,b);    
+        return mcm;
+    }
+	/**
+	 * Devuleve el valor mayor de dos numero de tipo long
+	 * @param valorA
+	 * @param valorB
+	 * @return
+	 */
+	private static long numeroMayor(long valorA, long valorB){
+		return (valorA > valorB) ? valorA : valorB;
+	}
+	/**
+	 * Devuleve el valor menor de dos numero de tipo long
+	 * @param valorA
+	 * @param valorB
+	 * @return
+	 */
+	private static long numeroMenor(long valorA, long valorB){
+		return (valorA > valorB) ? valorB : valorA;
+	}
+	/**
+	 * MÃ©todo que calculo el maximo comun divisor. La versión recusiva.
+	 * @param numA
+	 * @param numB
+	 * @return el valor mcd calculado.
+	 */
+	public static long gcdR(long valorA, long valorB){
+		if(valorB == 0) {
+			return valorA;
+		}else {
+			return gcd(valorB,valorA%valorB);
+		}
+	}
+	/**
+	 * Métod para obetenr el signo de la fracción.
+	 * @return 1 si fracción es positiva , -1 negativa y 0 en otro caso.
+	 */
+	public long sign() {
+		return (numerador == 0)? 0L : ( (numerador > 0)? 1L : -1L );
+	}
 	// si metemos 5.25 lo puedo tranforamr en 525/100 utilizar exprecion regurlas. -?/b3/b//b-?5/b si se introduce en extring.
-	// 3/5+ esti es yb error signo + mal colocado.
 	// parseFraction(). si determinao si es una fraccion o n
 	// simplificar y hacer sumas y la resta par ano se me desborde los numero.
 	// si tengo un enterr 3/5 + 3 = 18/5 por que se k 3 es un entero. se imprementa mas rapido.
@@ -88,7 +240,6 @@ public class Fraccion {
 	// 0 es tanto positivo o negativo le pongo 0 y dpesu si se utliza se multiplica por 0 y dara 0 todo queda perfecto.
 	// simplificar es un metodo private.
 	// normaliza private de uso dinnterno y siempre se utliza.
-	Fraccion(long n){}
 	/**
 	 * MÃ©todo de accesos al dato numerador.
 	 * @return el numerador.
@@ -119,6 +270,35 @@ public class Fraccion {
 	public void setDenominador(long denominador) {
 		this.denominador = denominador;
 	}
+	/**
+	 * Método que modifica la fracion  con los valores de la fraccion
+	 * que se le pasa.
+	 * @param nueva
+	 */
+	public void setFraction(Fraccion nuevaF) {
+		numerador = nuevaF.getNumerador();
+    	denominador = nuevaF.getDenominador();
+	}
 	
+	public double ratio() {
+		return (double)numerador / (double)denominador;
+	}
+	
+	/** (sin Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return (denominador != 1) ? numerador + "/" + denominador : numerador + "";
+	}
+	/**
+	 * Método para imprimir una fraccion.
+	 */
+	 public void print() {
+		 System.out.println(this);
+	 }
+	
+	 add, minus, multiply y divide
+	 public Fraccion add
 	
 }
--- src/fraccion/FraccionTest.java
+++ src/fraccion/FraccionTest.java
@@ -14,6 +14,14 @@ public class FraccionTest {
 		frac = new Fraccion(5,2);
 	}
 	@Test
+	public void gcdTest() {
+		assertEquals(4,Fraccion.gcd(28,8));
+		assertEquals(4,Fraccion.gcd(-8,28));
+		assertEquals(-4,Fraccion.gcd(-8,-28));
+		assertEquals(-4,Fraccion.gcd(8,-28));
+	}
+	
+	@Test
 	public void test() {
 		assertNotNull(frac);
 	}
@@ -40,11 +48,150 @@ public class FraccionTest {
 	}
 	@Test
 	public void verQueSeGuardaValorSimplificado(){
-		   Fraccion nueva = new Fraccion(15000, 500); 
-		   assertEquals(5,frac.getNumerador());
-		   assertEquals(2,frac.getDenominador());
+		   Fraccion fracNueva = new Fraccion(15000, 501); 
+		   assertEquals(5000,fracNueva.getNumerador());
+		   assertEquals(167,fracNueva.getDenominador());
+		   // Si no se puede simplificar
+		   Fraccion fracNew = new Fraccion(5000, 167); 
+		   assertEquals(5000,fracNew.getNumerador());
+		   assertEquals(167,fracNew.getDenominador());
+		   // Si se introuduce un valor en en numerado de 0
+		   Fraccion fracOtro = new Fraccion(0, 167); 
+		   assertEquals(0,fracOtro.getNumerador());
+		   assertEquals(1,fracOtro.getDenominador());
 		   
 	}
-
-
+	@Test
+	public void verSiEsUnDecimalTest(){
+		assertTrue(Fraccion.isDecimal("2.545"));
+		assertTrue(Fraccion.isDecimal("0.545"));
+		assertTrue(Fraccion.isDecimal("-30.545"));
+		assertTrue(Fraccion.isDecimal("+30.545"));
+		assertTrue(Fraccion.isDecimal("+30,545"));
+		assertTrue(Fraccion.isDecimal("+30 ,  545"));
+		assertFalse(Fraccion.isDecimal("155"));
+		assertFalse(Fraccion.isDecimal("-155"));
+		// Si hay fallo al escribir
+		assertTrue(Fraccion.isDecimal("2.5   45"));
+		assertTrue(Fraccion.isDecimal("2.545   "));
+		assertTrue(Fraccion.isDecimal("   2.545"));
+		assertTrue(Fraccion.isDecimal(" +2   .545"));
+		
+	}
+	
+	@Test
+	public void verSiEsEnteroTest(){
+		assertFalse(Fraccion.isEntero("2.545"));
+		assertFalse(Fraccion.isEntero("0.545"));
+		assertFalse(Fraccion.isEntero("-30.545"));
+		assertFalse(Fraccion.isEntero("+30.545"));
+		assertTrue(Fraccion.isEntero("155"));
+		assertTrue(Fraccion.isEntero("-155"));
+		assertTrue(Fraccion.isEntero("+155"));
+		// si hay espacio o trabuladcion o otra cosa.
+		assertTrue(Fraccion.isEntero(" + 155 "));
+	}
+	
+	@Test
+	public void verSiEsFraccionTest(){
+		assertFalse(Fraccion.isFraccion("23"));
+		assertFalse(Fraccion.isFraccion("0.545"));
+		assertFalse(Fraccion.isFraccion("-30.545"));
+		assertFalse(Fraccion.isFraccion("+30,545"));
+		assertFalse(Fraccion.isFraccion("+30, 545"));
+		assertTrue(Fraccion.isFraccion("155/3"));
+		assertTrue(Fraccion.isFraccion("-155/4"));
+		assertTrue(Fraccion.isFraccion("+155/-1544"));
+	
+		// si hay espacio o trabuladcion o otra cosa.
+		assertTrue(Fraccion.isFraccion("-15 5/-15  44"));
+		assertTrue(Fraccion.isFraccion("-  155/-  1544"));
+		assertTrue(Fraccion.isFraccion("-  155/-  1544      "));
+		assertTrue(Fraccion.isFraccion("        +  155/-  1544      "));
+	}
+	@Test
+	public void verLosConstruTest(){
+		// Constructor por defecto.
+		Fraccion aux0 = new Fraccion();
+		assertEquals(0, aux0.getNumerador());
+		assertEquals(1, aux0.getDenominador());
+		
+		// Constructro se le pasa un long.
+		Fraccion aux1 = new Fraccion(-100000000);
+		assertEquals(-100000000, aux1.getNumerador());
+		assertEquals(1, aux1.getDenominador());
+		
+		// Construcotr se le pasa n y d.
+		Fraccion aux2= new Fraccion(-10,2);
+		assertEquals(-5, aux2.getNumerador());
+		assertEquals(1, aux2.getDenominador());
+		
+		// Constructor que se le pasa un double.
+		Fraccion aux3 = new Fraccion(-15.02);
+		assertEquals(-751, aux3.getNumerador());
+		assertEquals(50, aux3.getDenominador());
+	}
+	// vemos esl funcionamiento del congrucco que sele pasa un stirng.
+	@Test
+	public void verConstructorStringTest(){
+			
+		// Constructro se le pasa un long.
+		Fraccion aux1 = new Fraccion("-100000000");
+		assertEquals(-100000000, aux1.getNumerador());
+		assertEquals(1, aux1.getDenominador());
+		
+		// Construcotr se le pasa n y d.
+		Fraccion aux2= new Fraccion("-10/2");
+		assertEquals(-5, aux2.getNumerador());
+		assertEquals(1, aux2.getDenominador());
+		
+		// Constructor que se le pasa un double.
+		Fraccion aux3 = new Fraccion("-15.02");
+		assertEquals(-751, aux3.getNumerador());
+		assertEquals(50, aux3.getDenominador());
+	}
+	@Test
+	public void verSignoTest(){
+		Fraccion aux = new Fraccion();
+		assertEquals(0, aux.sign());
+		
+		Fraccion aux1 = new Fraccion(+15,2);
+		assertEquals(1, aux1.sign());
+		
+		Fraccion aux2 = new Fraccion(-15,2);
+		assertEquals(-1, aux2.sign());
+	}
+	
+	@Test
+	public void verChangeSignoTest(){
+		Fraccion aux1 = new Fraccion(+15,2);
+		assertEquals(1, aux1.sign());
+	    aux1.changeSing();
+		assertEquals(-1, aux1.sign());
+	}
+	@Test
+	public void verStringTest(){
+		Fraccion aux1 = new Fraccion(+15,2);
+		assertEquals("15/2", aux1.toString());
+		aux1.setDenominador(1);
+		assertEquals("15", aux1.toString());
+	}
+	@Test
+	public void verRatioTest(){
+		Fraccion aux1 = new Fraccion(15,2);
+		assertEquals(7.5, aux1.ratio(),0.01);
+	}
+	@Test
+	public void verCloneTest(){
+		Fraccion aux1 = new Fraccion(15,2);
+		Fraccion aux2 = (Fraccion) aux1.clone();
+		assertNotSame(aux1, aux2);
+		assertEquals(aux1.toString(), aux2.toString());
+	}
+	@Test
+	public void verMcMTest(){
+		
+		assertEquals(84, Fraccion.lcm(28,12));
+		assertEquals(140, Fraccion.lcm(28,35));
+	}
 }
